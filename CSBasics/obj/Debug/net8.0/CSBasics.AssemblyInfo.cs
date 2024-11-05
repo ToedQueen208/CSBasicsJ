4880141c0b5cc0279aca2b161d7a0ccf5a744402
Jayla (they/them) <jakeclegg99@gmail.com>
2024-11-05 15:02:27 +0000

    adasd

--- CSBasics/obj/Debug/net8.0/CSBasics.AssemblyInfo.cs
+++ CSBasics/obj/Debug/net8.0/CSBasics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSBasics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe02ffe69d47a9f039ed06dde64699b4641567c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198696cbe7cfdb8c7bc61706093b908cf0c7f021")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSBasics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSBasics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
